3480fd656a3774ed5804216f1695825a80e7a3f5
William Yang <williamyang93@gmail.com>
2019-01-15 23:19:18 -0800

    Updates

---
+++ precache-manifest.a2ef50778e37ba577c0c9a34b6a02efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-checklist/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "bbad7d81b3b5d99e959f",
+    "url": "/starwars-checklist/static/js/runtime~main.bbad7d81.js"
+  },
+  {
+    "revision": "f815ed7c7bcbc38edbc2",
+    "url": "/starwars-checklist/static/js/main.f815ed7c.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/starwars-checklist/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "f815ed7c7bcbc38edbc2",
+    "url": "/starwars-checklist/static/css/main.8587f88d.chunk.css"
+  },
+  {
+    "revision": "750313e6b537920af49d04b20c067ab1",
+    "url": "/starwars-checklist/index.html"
+  }
+];
